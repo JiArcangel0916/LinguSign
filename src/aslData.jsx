02a1057ea1556ce237222de62e9389e57e0bd312
JiArcangel0916 <arcangelalbrecht@gmail.com>
2026-04-16 23:08:18 +0800

    add: added aslData for dictionary and learn

---
+++ src/aslData.jsx
@@ -0,0 +1,179 @@
+{/* 
+    These data will be used by the frontend
+    to get and load specific words/alphabets
+    to be displayed in the dictionary tab and used in the learn tab
+*/}
+export const ALPHABET_DICT = [
+    { id: 1, letter: "A", path: "<path_to_img>" },
+    { id: 2, letter: "B", path: "<path_to_img>" },
+    { id: 3, letter: "C", path: "<path_to_img>" },
+    { id: 4, letter: "D", path: "<path_to_img>" },
+    { id: 5, letter: "E", path: "<path_to_img>" },
+    { id: 6, letter: "F", path: "<path_to_img>" },
+    { id: 7, letter: "G", path: "<path_to_img>" },
+    { id: 8, letter: "H", path: "<path_to_img>" },
+    { id: 9, letter: "I", path: "<path_to_img>" },
+    { id: 10, letter: "J", path: "<path_to_img>" },
+    { id: 11, letter: "K", path: "<path_to_img>" },
+    { id: 12, letter: "L", path: "<path_to_img>" },
+    { id: 13, letter: "M", path: "<path_to_img>" },
+    { id: 14, letter: "N", path: "<path_to_img>" },
+    { id: 15, letter: "O", path: "<path_to_img>" },
+    { id: 16, letter: "P", path: "<path_to_img>" },
+    { id: 17, letter: "Q", path: "<path_to_img>" },
+    { id: 18, letter: "R", path: "<path_to_img>" },
+    { id: 19, letter: "S", path: "<path_to_img>" },
+    { id: 20, letter: "T", path: "<path_to_img>" },
+    { id: 21, letter: "U", path: "<path_to_img>" },
+    { id: 22, letter: "V", path: "<path_to_img>" },
+    { id: 23, letter: "W", path: "<path_to_img>" },
+    { id: 24, letter: "X", path: "<path_to_img>" },
+    { id: 25, letter: "Y", path: "<path_to_img>" },
+    { id: 26, letter: "Z", path: "<path_to_img>" },
+]
+
+export const WORD_DICT = [
+    {
+        id: 1, category: "Actions", words: [
+            { wordId: 1, word: "", path: "<path_to_vid>" },
+            { wordId: 2, word: "", path: "<path_to_vid>" },
+            { wordId: 3, word: "", path: "<path_to_vid>" },
+            { wordId: 4, word: "", path: "<path_to_vid>" },
+            { wordId: 5, word: "", path: "<path_to_vid>" },
+            { wordId: 6, word: "", path: "<path_to_vid>" },
+            { wordId: 7, word: "", path: "<path_to_vid>" },
+            { wordId: 8, word: "", path: "<path_to_vid>" },
+            { wordId: 9, word: "", path: "<path_to_vid>" },
+            { wordId: 10, word: "", path: "<path_to_vid>" }]
+    },
+    {
+        id: 2, category: "Adjectives", words: [
+            { wordId: 1, word: "", path: "<path_to_vid>" },
+            { wordId: 2, word: "", path: "<path_to_vid>" },
+            { wordId: 3, word: "", path: "<path_to_vid>" },
+            { wordId: 4, word: "", path: "<path_to_vid>" },
+            { wordId: 5, word: "", path: "<path_to_vid>" },
+            { wordId: 6, word: "", path: "<path_to_vid>" },
+            { wordId: 7, word: "", path: "<path_to_vid>" },
+            { wordId: 8, word: "", path: "<path_to_vid>" },
+            { wordId: 9, word: "", path: "<path_to_vid>" },
+            { wordId: 10, word: "", path: "<path_to_vid>" }]
+    },
+    {
+        id: 3, category: "Animals", words: [
+            { wordId: 1, word: "", path: "<path_to_vid>" },
+            { wordId: 2, word: "", path: "<path_to_vid>" },
+            { wordId: 3, word: "", path: "<path_to_vid>" },
+            { wordId: 4, word: "", path: "<path_to_vid>" },
+            { wordId: 5, word: "", path: "<path_to_vid>" },
+            { wordId: 6, word: "", path: "<path_to_vid>" },
+            { wordId: 7, word: "", path: "<path_to_vid>" },
+            { wordId: 8, word: "", path: "<path_to_vid>" },
+            { wordId: 9, word: "", path: "<path_to_vid>" },
+            { wordId: 10, word: "", path: "<path_to_vid>" }]
+    },
+    {
+        id: 4, category: "Color", words: [
+            { wordId: 1, word: "", path: "<path_to_vid>" },
+            { wordId: 2, word: "", path: "<path_to_vid>" },
+            { wordId: 3, word: "", path: "<path_to_vid>" },
+            { wordId: 4, word: "", path: "<path_to_vid>" },
+            { wordId: 5, word: "", path: "<path_to_vid>" },
+            { wordId: 6, word: "", path: "<path_to_vid>" },
+            { wordId: 7, word: "", path: "<path_to_vid>" },
+            { wordId: 8, word: "", path: "<path_to_vid>" },
+            { wordId: 9, word: "", path: "<path_to_vid>" },
+            { wordId: 10, word: "", path: "<path_to_vid>" }]
+    },
+    {
+        id: 5, category: "Emergency", words: [
+            { wordId: 1, word: "", path: "<path_to_vid>" },
+            { wordId: 2, word: "", path: "<path_to_vid>" },
+            { wordId: 3, word: "", path: "<path_to_vid>" },
+            { wordId: 4, word: "", path: "<path_to_vid>" },
+            { wordId: 5, word: "", path: "<path_to_vid>" },
+            { wordId: 6, word: "", path: "<path_to_vid>" },
+            { wordId: 7, word: "", path: "<path_to_vid>" },
+            { wordId: 8, word: "", path: "<path_to_vid>" },
+            { wordId: 9, word: "", path: "<path_to_vid>" },
+            { wordId: 10, word: "", path: "<path_to_vid>" }]
+    },
+    {
+        id: 6, category: "Family", words: [
+            { wordId: 1, word: "", path: "<path_to_vid>" },
+            { wordId: 2, word: "", path: "<path_to_vid>" },
+            { wordId: 3, word: "", path: "<path_to_vid>" },
+            { wordId: 4, word: "", path: "<path_to_vid>" },
+            { wordId: 5, word: "", path: "<path_to_vid>" },
+            { wordId: 6, word: "", path: "<path_to_vid>" },
+            { wordId: 7, word: "", path: "<path_to_vid>" },
+            { wordId: 8, word: "", path: "<path_to_vid>" },
+            { wordId: 9, word: "", path: "<path_to_vid>" },
+            { wordId: 10, word: "", path: "<path_to_vid>" }]
+    },
+    {
+        id: 7, category: "General", words: [
+            { wordId: 1, word: "", path: "<path_to_vid>" },
+            { wordId: 2, word: "", path: "<path_to_vid>" },
+            { wordId: 3, word: "", path: "<path_to_vid>" },
+            { wordId: 4, word: "", path: "<path_to_vid>" },
+            { wordId: 5, word: "", path: "<path_to_vid>" },
+            { wordId: 6, word: "", path: "<path_to_vid>" },
+            { wordId: 7, word: "", path: "<path_to_vid>" },
+            { wordId: 8, word: "", path: "<path_to_vid>" },
+            { wordId: 9, word: "", path: "<path_to_vid>" },
+            { wordId: 10, word: "", path: "<path_to_vid>" }]
+    },
+    {
+        id: 8, category: "Months", words: [
+            { wordId: 1, word: "", path: "<path_to_vid>" },
+            { wordId: 2, word: "", path: "<path_to_vid>" },
+            { wordId: 3, word: "", path: "<path_to_vid>" },
+            { wordId: 4, word: "", path: "<path_to_vid>" },
+            { wordId: 5, word: "", path: "<path_to_vid>" },
+            { wordId: 6, word: "", path: "<path_to_vid>" },
+            { wordId: 7, word: "", path: "<path_to_vid>" },
+            { wordId: 8, word: "", path: "<path_to_vid>" },
+            { wordId: 9, word: "", path: "<path_to_vid>" },
+            { wordId: 10, word: "", path: "<path_to_vid>" }]
+    },
+    {
+        id: 9, category: "Places", words: [
+            { wordId: 1, word: "", path: "<path_to_vid>" },
+            { wordId: 2, word: "", path: "<path_to_vid>" },
+            { wordId: 3, word: "", path: "<path_to_vid>" },
+            { wordId: 4, word: "", path: "<path_to_vid>" },
+            { wordId: 5, word: "", path: "<path_to_vid>" },
+            { wordId: 6, word: "", path: "<path_to_vid>" },
+            { wordId: 7, word: "", path: "<path_to_vid>" },
+            { wordId: 8, word: "", path: "<path_to_vid>" },
+            { wordId: 9, word: "", path: "<path_to_vid>" },
+            { wordId: 10, word: "", path: "<path_to_vid>" }]
+    },
+    {
+        id: 10, category: "School", words: [
+            { wordId: 1, word: "", path: "<path_to_vid>" },
+            { wordId: 2, word: "", path: "<path_to_vid>" },
+            { wordId: 3, word: "", path: "<path_to_vid>" },
+            { wordId: 4, word: "", path: "<path_to_vid>" },
+            { wordId: 5, word: "", path: "<path_to_vid>" },
+            { wordId: 6, word: "", path: "<path_to_vid>" },
+            { wordId: 7, word: "", path: "<path_to_vid>" },
+            { wordId: 8, word: "", path: "<path_to_vid>" },
+            { wordId: 9, word: "", path: "<path_to_vid>" },
+            { wordId: 10, word: "", path: "<path_to_vid>" }]
+    },
+    {
+        id: 11, category: "Time", words: [
+            { wordId: 1, word: "", path: "<path_to_vid>" },
+            { wordId: 2, word: "", path: "<path_to_vid>" },
+            { wordId: 3, word: "", path: "<path_to_vid>" },
+            { wordId: 4, word: "", path: "<path_to_vid>" },
+            { wordId: 5, word: "", path: "<path_to_vid>" },
+            { wordId: 6, word: "", path: "<path_to_vid>" },
+            { wordId: 7, word: "", path: "<path_to_vid>" },
+            { wordId: 8, word: "", path: "<path_to_vid>" },
+            { wordId: 9, word: "", path: "<path_to_vid>" },
+            { wordId: 10, word: "", path: "<path_to_vid>" }]
+    }
+]
